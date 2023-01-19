b0a82657af8f340f418295bae158f5132294ce4b
Nick Spinale <nick@nickspinale.com>
2023-01-19 14:03:38 +0000

    HACK: build_sdk.py: Skip building for other configs

--- build_sdk.py
+++ build_sdk.py
@@ -54,33 +54,6 @@ class ConfigInfo:
 
 
 SUPPORTED_BOARDS = (
-    BoardInfo(
-        name="tqma8xqp1gb",
-        gcc_cpu="cortex-a35",
-        loader_link_address=0x80280000,
-        kernel_options={
-            "KernelPlatform": "tqma8xqp1gb",
-            "KernelIsMCS": True,
-            "KernelArmExportPCNTUser": True,
-        },
-        examples={
-            "ethernet": Path("example/tqma8xqp1gb/ethernet")
-        }
-    ),
-    BoardInfo(
-        name="zcu102",
-        gcc_cpu="cortex-a53",
-        loader_link_address=0x40000000,
-        kernel_options={
-            "KernelPlatform": "zynqmp",
-            "KernelARMPlatform": "zcu102",
-            "KernelIsMCS": True,
-            "KernelArmExportPCNTUser": True,
-        },
-        examples={
-            "hello": Path("example/zcu102/hello")
-        }
-    ),
     # BoardInfo(
     #     name="imx8mm",
     #     gcc_cpu="cortex-a53",
@@ -110,11 +83,6 @@ SUPPORTED_BOARDS = (
 )
 
 SUPPORTED_CONFIGS = (
-    ConfigInfo(
-        name="release",
-        debug=False,
-        kernel_options={},
-    ),
     ConfigInfo(
         name="debug",
         debug=True,
