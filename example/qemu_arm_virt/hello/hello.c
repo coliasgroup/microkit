patrick-forks/sel4cp-patrick

Initial support for QEMU's ARM virt platform
Run with the following command:
qemu-system-aarch64 -machine virt -cpu cortex-a53 -serial mon:stdio -device loader,file=tmp_build/loader.img,addr=0x70000000,cpu-num=0 -m size=1G -display none

example/qemu_arm_virt/hello/hello.c
@@ -1,0 +1,18 @@
+/*
+ * Copyright 2021, Breakaway Consulting Pty. Ltd.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stdint.h>
+#include <sel4cp.h>
+
+void
+init(void)
+{
+    sel4cp_dbg_puts("hello, world\n");
+}
+
+void
+notified(sel4cp_channel ch)
+{
+}

coliasgroup/microkit

example/qemu_arm_virt/hello/hello.c
@@ -1,0 +1,18 @@
+/*
+ * Copyright 2021, Breakaway Consulting Pty. Ltd.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stdint.h>
+#include <microkit.h>
+
+void
+init(void)
+{
+    microkit_dbg_puts("hello, world\n");
+}
+
+void
+notified(microkit_channel ch)
+{
+}