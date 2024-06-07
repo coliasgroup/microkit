c21ee4eab68396a3ba0a9533296c8d6840eb7dfc
Nick Spinale <nick@nickspinale.com>
2024-06-07 09:15:37 +0000

    HACK: build_sdk.py: skip building tool

--- build_sdk.py
+++ build_sdk.py
@@ -463,9 +463,6 @@ def main() -> None:
 
     tool_target = root_dir / "bin" / "microkit"
 
-    test_tool()
-    build_tool(tool_target, args.tool_target_triple)
-
     if not args.skip_docs:
         build_doc(root_dir)
 
